bb3ce6e965382cd445fb5393bbba28f02b45dc7b
akramksb <akram.kssiba@gmail.com>
2022-03-12 17:31:28 +0100

    solve puzzle with A star

--- game.py
+++ game.py
@@ -1,18 +1,6 @@
 from copy import deepcopy
 from settings import *
 
-class Node:
-    def __init__(self, state, parent_node, g, h, move):
-        self.state = state
-        self.parent_node = parent_node
-        self.g = g
-        self.h = h
-        self.move = move
-    
-    def f(self):
-        return self.g + self.h
-
-
 class Puzzle:
     def __init__(self, current_state, goal_state):
         self.state = current_state
@@ -25,13 +13,15 @@ class Puzzle:
                     return ( row_index, col_index )
 
     def isGoalState(self):
-        for row_index in range( len(self.state) ):
-            for col_index in range( len( self.state[0] )):
-                if self.state[row_index][col_index] != self.goal_state[row_index][col_index]:
-                    return False
-        return True
+        return self.state == self.goal_state
+        # for row_index in range( len(self.state) ):
+        #     for col_index in range( len( self.state[0] )):
+        #         if self.state[row_index][col_index] != self.goal_state[row_index][col_index]:
+        #             return False
+        # return True
 
     def move(self, direction_str):
+        ''' 0 : not a legal move , 1 : legal move '''
         direction = DIRECTIONS[direction_str]
         # get empty square coordinates
         emptySquare = self.findEmptySquare()
@@ -39,13 +29,33 @@ class Puzzle:
         newEmptySqaure = [ emptySquare[0]+direction[0],
                         emptySquare[1]+direction[1] ]
         # check is legal move
-        if newEmptySqaure[0] < 0 or newEmptySqaure[0] >= len(self.state)\
-            or newEmptySqaure[1] < 0 or newEmptySqaure[1] >= len(self.state[0]):
-            return
-        # swap empty square oldPos with newPos
-        # self.state[ newEmptySqaure[0] ][ newEmptySqaure[1] ] = self.state[ emptySquare[0] ][emptySquare[1]]
-        self.state[ newEmptySqaure[0] ][ newEmptySqaure[1] ], self.state[ emptySquare[0] ][emptySquare[1]]\
-            = self.state[ emptySquare[0] ][emptySquare[1]], self.state[ newEmptySqaure[0] ][ newEmptySqaure[1] ]
+        if 0 <= newEmptySqaure[0] < len(self.state)\
+            and 0 <= newEmptySqaure[1] < len(self.state[0]) :
+            # swap empty square oldPos with newPos
+            self.state[ newEmptySqaure[0] ][ newEmptySqaure[1] ], self.state[ emptySquare[0] ][emptySquare[1]]\
+                = self.state[ emptySquare[0] ][emptySquare[1]], self.state[ newEmptySqaure[0] ][ newEmptySqaure[1] ]
+            return 1
+        return 0
+
+    # DELETE THIS !
+    def getNextMoves(self):
+        next_moves = []
+        for direction in DIRECTIONS :
+            direction = DIRECTIONS[direction_str]
+            # get empty square coordinates
+            emptySquare = self.findEmptySquare()
+            # get empty square new coordinates
+            newEmptySqaure = [ emptySquare[0]+direction[0],
+                            emptySquare[1]+direction[1] ]
+            # check is legal move
+            if 0 <= newEmptySqaure[0] < len(self.state)\
+                and 0 <= newEmptySqaure[1] < len(self.state[0]) :
+                newState = deepcopy(self.state)
+                # swap empty square oldPos with newPos
+                newState[ newEmptySqaure[0] ][ newEmptySqaure[1] ], newState[ emptySquare[0] ][emptySquare[1]]\
+                    = newState[ emptySquare[0] ][emptySquare[1]], newState[ newEmptySqaure[0] ][ newEmptySqaure[1] ]
+                next_moves.append(newState)
+        return next_moves
     
     def show(self):
         for row in self.state:
@@ -58,8 +68,8 @@ class Puzzle:
 def main():
     input_to_dir = { "a":"LEFT", "s":"DOWN","d":"RIGHT","w":"UP" }
     puzzle = Puzzle(INITIAL_STATE, GOAL_STATE)
+    puzzle.show()
     while not puzzle.isGoalState():
-        puzzle.show()
         print()
         input_dir = input( ">" )
         print()
@@ -67,6 +77,7 @@ def main():
             print("move with w,a,s,d")
             continue
         puzzle.move( input_to_dir[input_dir] )
+        puzzle.show()
     
 if __name__ == "__main__":
     main()
---
+++ puzzle.py
@@ -0,0 +1,125 @@
+from copy import deepcopy
+from game import *
+
+class Node:
+    def __init__(self, state, parent_node, g, h, move):
+        self.state = state
+        self.parent_node = parent_node
+        self.g = g
+        self.h = h
+        self.move = move
+        self.id = 0
+        for row in self.state:
+            for col in row:
+                self.id = self.id*10 + col
+        # print( self.id )
+    
+    def f(self):
+        return self.g + self.h
+
+
+def h( state, goal_state = GOAL_STATE ):
+    score = 0
+    for row_index in range( len(state) ):
+            for col_index in range( len( state[0] )):
+                if state[row_index][col_index] != goal_state[row_index][col_index]:
+                    score += 1
+    return score
+
+def findEmptySquare(state):
+        for row_index in range( len(state)):
+            for col_index in range( len( state[0] )):
+                if state[row_index][col_index] == 0:
+                    return ( row_index, col_index )
+
+
+def getNextNodes( node : Node ):
+    next_nodes = []
+    for direction_str in DIRECTIONS :
+        direction = DIRECTIONS[direction_str]
+        # get empty square coordinates
+        emptySquare = findEmptySquare( node.state )
+        # get empty square new coordinates
+        newEmptySqaure = [ emptySquare[0]+direction[0],
+                        emptySquare[1]+direction[1] ]
+        # check is legal move
+        if 0 <= newEmptySqaure[0] < len(node.state) and 0 <= newEmptySqaure[1] < len(node.state[0]) :
+            new_state = deepcopy(node.state)
+            # swap empty square oldPos with newPos
+            new_state[ newEmptySqaure[0] ][ newEmptySqaure[1] ], new_state[ emptySquare[0] ][emptySquare[1]]\
+                = new_state[ emptySquare[0] ][emptySquare[1]], new_state[ newEmptySqaure[0] ][ newEmptySqaure[1] ]
+            # create node from state and add it to the list
+            new_node = Node( new_state, node, node.g+1, h( new_state ), direction_str )
+            next_nodes.append( new_node )
+    return next_nodes
+
+
+def getLowestCostNode( nodeSet ):
+    nodeList = list( nodeSet.values() )
+    if nodeList == [] : return
+
+    lowest_cost_node = nodeList[0]
+    lowest_cost = lowest_cost_node.f()
+    for node in nodeList:
+        cost = node.f()
+        if cost < lowest_cost:
+            lowest_cost = cost
+            lowest_cost_node = node
+
+    return lowest_cost_node
+
+def getFullPath( node : Node ):
+    path = []
+    while node.parent_node:
+        path.insert(0, node.move)
+        node = node.parent_node
+    return path
+
+
+def solvePuzzle( puzzle : Puzzle ):
+    root_node = Node( puzzle.state, None , 0, h(puzzle.state) , "" )
+    openSet = { root_node.id : root_node } 
+    closedSet = { }
+
+    while openSet != []:
+        node = getLowestCostNode( openSet )
+
+        if node.state == puzzle.goal_state:
+            return  getFullPath(node)
+
+        # add node to visited nodes
+        closedSet[ node.id ] = node
+
+        next_nodes = getNextNodes(node)
+        for n in next_nodes:
+            # check if node in open set
+            if n.id in openSet and openSet[n.id].f() < n.f() :
+                continue
+            
+            # check if node in closed set
+            if n.id in closedSet :
+                if closedSet[n.id].f() < n.f():
+                    continue
+                # remove node from closed set
+                del closedSet[n.id]
+
+            openSet[n.id] = n
+
+        # remove node from open nodes
+        del openSet[ node.id ]
+
+def printState(state):
+    for row in state:
+        for col in row:
+            if col == 0 : col = " "
+            print( col, end = " " )
+        print()
+    print()
+
+def main():
+    puzzle = Puzzle(INITIAL_STATE, GOAL_STATE)
+    path = solvePuzzle(puzzle)
+    print( path )
+
+if __name__ == "__main__":
+    main()
